57f011a86142af2a0c608d78eceba495ea85ab4e
Skhmelyuk <skhmelyuk1985@gmail.com>
2026-03-31 11:53:25 +0300

    lesson-05

--- components/Story.tsx
+++ components/Story.tsx
@@ -1,14 +1,4 @@
 import { styles } from "@/styles/feed.styles";
-<<<<<<< HEAD
-import { View, Text, TouchableOpacity } from "react-native";
-import { Image } from "expo-image";
-
-export default function Story({ story }: { story: any }) {
-  return (
-    <TouchableOpacity style={styles.storyWrapper}>
-      <View style={[styles.storyRing, !story.hasStory && styles.noStory]}>
-        <Image source={story.avatar} style={styles.storyAvatar} />
-=======
 import { View, Text, Image, TouchableOpacity } from "react-native";
 
 type Story = {
@@ -23,7 +13,6 @@ export default function Story({ story }: { story: Story }) {
     <TouchableOpacity style={styles.storyWrapper}>
       <View style={[styles.storyRing, !story.hasStory && styles.noStory]}>
         <Image source={{ uri: story.avatar }} style={styles.storyAvatar} />
->>>>>>> lesson-05
       </View>
       <Text style={styles.storyUsername}>{story.username}</Text>
     </TouchableOpacity>
